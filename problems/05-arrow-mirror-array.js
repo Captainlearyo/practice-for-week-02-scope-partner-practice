ab2c407aae512ae8c522f88e7b34296b4a74d964
Adam Tull <adamchristoph18@gmail.com>
2022-12-02 16:44:57 -0500

    arrow-mirror-arr

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -3,15 +3,20 @@ Write a function `arrowMirrorArray(array)` that takes in an array as an
 argument and returns a new array "mirrored" as shown in the examples.
 
 Write this function using an arrow function!
-
-Examples:
-
-arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
-arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
 // Your code here
+const arrowMirrorArray = arr => {
+  let mirror = [];
+  for (let i = arr.length -1; i >= 0; i--) {
+    mirror.push(arr[i])
+  }
+  return arr.concat(mirror);
+}
 
+// Examples:
+console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+console.log(arrowMirrorArray(['a', 'b', 'c', 'd'])); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
